f4cc0c2e9e459ff558ebd279b15ac967634d1e36
Daniyal Khan <dk720820@gmail.com>
2021-11-06 18:16:22 +0500

    Initial commit

---
+++ Student Management System/About.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Student_Management_System
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Student_Management_System.Properties.Resources.Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(300, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(155, 146);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(62, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(685, 56);
+            this.label1.TabIndex = 1;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(62, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(380, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Address: 47 Sheppard Ave E, North York, ON M2N 1M4";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(62, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(272, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Phone: (416) 963-1211";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(62, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(272, 23);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Province: Ontario";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(62, 317);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 23);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "info@ibtcollege.com";
+            this.label5.Click += new System.EventHandler(this.Label5_Click);
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "About";
+            this.Text = "About";
+            this.Load += new System.EventHandler(this.About_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Student Management System/About.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Management_System
+{
+    public partial class About : Form
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void About_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
